42d28f2839437254184d98e15f25fd2108f674ae
chosm7489 <chosm7489@gmail.com>
2022-04-18 19:44:41 -0400

    div

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import './App.css';
 function App() {
   return (
     <div class = 'column'>
-        <div class = 'row1'> hello </div>
-        <div class = 'row2'> hello </div>
+        <div class = 'row1'> 
+            <p>
+              <img src="https://img.icons8.com/windows/32/000000/settings-system-daydream.png"/>
+              <img src="https://img.icons8.com/material-outlined/24/000000/like--v1.png"/>
+              Heardle
+              <img src="https://img.icons8.com/material-outlined/24/000000/high-connection.png"/>
+              <img src="https://img.icons8.com/ios-glyphs/30/000000/ask-question.png"/>
+              
+            </p>
+        </div>
+        <div class = 'row2'>
+            <div class ='songname'>          
+              <div class = "choice"></div>
+              <div class = "choice"></div>
+              <div class = "choice"></div>
+              <div class = "choice"></div>
+            </div>
+        </div>
         <div class = 'row3'> hello </div>
     </div>
   );
