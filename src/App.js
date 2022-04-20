aad72dfc916185a62f546c627ea78abe70787590
chosm7489 <chosm7489@gmail.com>
2022-04-20 17:27:51 -0400

    audio tag

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             <p>
               <img src="https://img.icons8.com/windows/32/000000/settings-system-daydream.png"/>
               <img src="https://img.icons8.com/material-outlined/24/000000/like--v1.png"/>
-              Heardle
+              Muse
               <img src="https://img.icons8.com/material-outlined/24/000000/high-connection.png"/>
               <img src="https://img.icons8.com/ios-glyphs/30/000000/ask-question.png"/>
               
@@ -22,7 +22,14 @@ function App() {
               <div class = "choice"></div>
             </div>
         </div>
-        <div class = 'row3'> hello </div>
+        <div class = 'row3'>
+          <audio controls>
+            <source src="http://localhost:3000/ImpactModerato.mp3" type="audio/mp3" />
+          </audio><br></br>
+          <input type="text"></input><br></br>
+          <input type="button"></input> 
+          <input type='button'></input>
+        </div>
     </div>
   );
 }
